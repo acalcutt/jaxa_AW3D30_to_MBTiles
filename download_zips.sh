a9856a0a07ccdb44e3ff85c2279e691fabf68685
acalcutt <acalcutt@techidiots.net>
2021-03-12 11:42:22 -0500

    v1 test

---
+++ download_zips.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mywget()
+{
+	wget --user 'jaxa_account' --password 'jaxa_password' --post-data 'username=jaxa_account&password=jaxa_password' "$1"
+}
+
+export -f mywget
+
+# run wget in parallel using 8 thread/connection
+xargs -P 8 -n 1 -I {} bash -c "mywget '{}'" < zip_file_list.txt
