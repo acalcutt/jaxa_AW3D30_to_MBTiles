479b1a66b0cc589e6f8306ba76a397c92b57aa3d
acalcutt <acalcutt@techidiots.net>
2021-03-12 11:43:50 -0500

    removed jpeg formatting

--- aw3d2mbtiles.sh
+++ aw3d2mbtiles.sh
@@ -10,5 +10,5 @@ mbtilesfile=${OUTPUT_DIR}/out.mbtiles
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
 
 gdalbuildvrt -overwrite -srcnodata -9999 -vrtnodata -9999 ${vrtfile} ${INPUT_DIR}/*_DSM.tif
-gdal_translate ${vrtfile} ${mbtilesfile} -of MBTILES -co TILE_FORMAT=JPEG -tr 1 -1
+gdal_translate ${vrtfile} ${mbtilesfile} -of MBTILES
 gdaladdo -r average ${mbtilesfile} 2 4 8 16
