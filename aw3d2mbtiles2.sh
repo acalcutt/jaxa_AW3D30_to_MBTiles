5a970f232e2b6705807aeb2db81f9f4006637acc
acalcutt <acalcutt@techidiots.net>
2021-03-25 11:51:14 -0400

    current mbtiles export

--- aw3d2mbtiles2.sh
+++ aw3d2mbtiles2.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
 
-INPUT_DIR=./merged2
-OUTPUT_DIR=/opt/aw3d30_srtmhgt
-vrtfile=./input_merged_hsc.vrt
+INPUT_DIR=/opt/aw3d30_srtmhgt/merged4/hsc
+OUTPUT_DIR=/opt/aw3d30_srtmhgt/output
+vrtfile=${OUTPUT_DIR}/jaxa_hillshade.vrt
 mbtilesfile=${OUTPUT_DIR}/jaxa_hillshade.mbtiles
-tiffile=${OUTPUT_DIR}/input_merged_hsc.tif
+#tiffile=${OUTPUT_DIR}/input_merged_hsc.tif
 
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
 
-#gdalbuildvrt -overwrite -srcnodata -9999 -vrtnodata -9999 ${vrtfile} ${INPUT_DIR}/*_HSC.tif
-#gdal_translate ${vrtfile} ${mbtilesfile} -of MBTILES 
+echo "Builing VRT"
+gdalbuildvrt -overwrite -srcnodata -9999 -vrtnodata -9999 ${vrtfile} ${INPUT_DIR}/*.tif
+echo "Import VRT into MBTiles"
+gdal_translate ${vrtfile} ${mbtilesfile} -of MBTILES 
 #gdaladdo -r average ${mbtilesfile} 2 4 8 16
 #rm ${mbtilesfile}
-#cp ${mbtilesfile}.orig ${mbtilesfile}
-#gdaladdo ${mbtilesfile} 2 4 8 16 32 64 128 256 512 1024
 #gdal2mbtiles -v --min-resolution 1 --max-resolution 12 --png8 4 --name "JAXA Hillshade" ${vrtfile} ${mbtilesfile}
 #gdal_translate ${vrtfile} ${tiffile} -of GTiff -co BIGTIFF=IF_SAFER -co NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 1024 -co COMPRESS=LZW -co PREDICTOR=2
-gdal2mbtiles -v --min-resolution 1 --max-resolution 12 --png8 4 --name "JAXA Hillshade" ${tiffile} ${mbtilesfile}
+#gdal2mbtiles -v --min-resolution 1 --max-resolution 12 --png8 4 --name "JAXA Hillshade" ${tiffile} ${mbtilesfile}
+echo "Backup Origional MBTiles file"
+cp ${mbtilesfile} ${mbtilesfile}.orig
+echo "Create MBTiles Overview"
+gdaladdo ${mbtilesfile}
