0f506333cec58d0921f553020481645fd93cea2f
acalcutt <acalcutt@techidiots.net>
2021-03-25 11:57:59 -0400

    color releif export

--- aw3d2mbtiles2c.sh
+++ aw3d2mbtiles2c.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 
-INPUT_DIR=./merged2
+INPUT_DIR=./images/crc
 OUTPUT_DIR=./output
-vrtfile=./input_merged_crc.vrt
-mbtilesfile=${OUTPUT_DIR}/colorized_color_jaxa.mbtiles
-
+vrtfile=${OUTPUT_DIR}/jaxa_color_releif.vrt
+mbtilesfile=${OUTPUT_DIR}/jaxa_color_releif.mbtiles
 
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
 
-gdalbuildvrt -overwrite -srcnodata -9999 -vrtnodata -9999 ${vrtfile} ${INPUT_DIR}/*_CRC.tif
+echo "Builing VRT"
+gdalbuildvrt -overwrite -srcnodata -9999 -vrtnodata -9999 ${vrtfile} ${INPUT_DIR}/*.tif
+echo "Import VRT into MBTiles"
 gdal_translate ${vrtfile} ${mbtilesfile} -of MBTILES 
-#gdaladdo -r average ${mbtilesfile} 2 4 8 16
-cp ${mbtilesfile} ${mbtilesfile}.orig
-gdaladdo ${mbtilesfile} 2 4 8 16 32 64 128 256 512 1024
+#echo "Backup Origional MBTiles file"
+#cp ${mbtilesfile} ${mbtilesfile}.orig
+echo "Create MBTiles Overview"
+gdaladdo ${mbtilesfile}
