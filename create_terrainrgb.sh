5d812f629a5234f6207a15c5119b85da10f6a729
acalcutt <acalcutt@techidiots.net>
2022-04-16 02:42:40 -0400

    default to Z12

--- create_terrainrgb.sh
+++ create_terrainrgb.sh
@@ -2,16 +2,16 @@
 
 INPUT_DIR=./input
 OUTPUT_DIR=./rio
-vrtfile=${OUTPUT_DIR}/jaxa_tilergb0-14.vrt
-mbtiles=${OUTPUT_DIR}/jaxa_tilergb0-14.mbtiles
-vrtfile2=${OUTPUT_DIR}/jaxa_tilergb0-14_warp.vrt
+vrtfile=${OUTPUT_DIR}/jaxa_tilergb0-12.vrt
+mbtiles=${OUTPUT_DIR}/jaxa_tilergb0-12.mbtiles
+vrtfile2=${OUTPUT_DIR}/jaxa_tilergb0-12_warp.vrt
 
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
 
 #rm rio/*
 gdalbuildvrt -overwrite -srcnodata -9999 -vrtnodata -9999 ${vrtfile} ${INPUT_DIR}/*_DSM.tif
 gdalwarp -r cubicspline -t_srs EPSG:3857 -dstnodata 0 -co COMPRESS=DEFLATE ${vrtfile} ${vrtfile2}
-rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 14 -j 24 --format png ${vrtfile2} ${mbtiles}
+rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 12 -j 24 --format png ${vrtfile2} ${mbtiles}
 
 #CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);
 sqlite3 ${mbtiles} 'CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);'
