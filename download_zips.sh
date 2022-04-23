585e83a131f4d68760dce764b7b5f61653aa38fe
acalcutt <acalcutt@techidiots.net>
2022-04-23 10:29:26 -0400

    Update download_zips.sh

--- download_zips.sh
+++ download_zips.sh
@@ -10,4 +10,5 @@ export -f mywget
 # run wget in parallel using 8 thread/connection
 xargs -P 8 -n 1 -I {} bash -c "mywget '{}'" < file_list_zip.txt
 
+#unzip the DSM tif files
 unzip -j download/\*.zip "*_DSM.tif" -d input/
