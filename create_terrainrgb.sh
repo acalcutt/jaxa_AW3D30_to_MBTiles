2a486fd1c53d0d0c21ec0a6b17a0b53d40de0760
Andrew Calcutt <acalcutt@techidiots.net>
2022-04-28 00:11:58 -0400

    add metadata

--- create_terrainrgb.sh
+++ create_terrainrgb.sh
@@ -15,3 +15,12 @@ rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 12 -j 24 --format png ${vrtfile2} 
 
 #CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);
 sqlite3 ${mbtiles} 'CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "jaxa_terrainrgb_0-12" WHERE name = "name" AND value = "";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify" WHERE name = "description";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "png" WHERE name = "format";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
+sqlite3 ${mbtiles} 'INSERT INTO metadata (name,value) VALUES(''attribution'',''<a href="https://earth.jaxa.jp/en/data/policy/">AW3D30 (JAXA)</a>'');'
+sqlite3 ${mbtiles} 'PRAGMA journal_mode=DELETE;'
+
+
