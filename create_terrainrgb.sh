4537c2696221fdd9f543dadc6a2d08383ba2d0c9
Darcy Buskermolen <darcy@quad-02.homenet.dbitech.bc.ca>
2023-01-01 07:35:10 -0800

    Clean up tile creation script update JAXA downloader script

--- create_terrainrgb.sh
+++ create_terrainrgb.sh
@@ -1,58 +1,186 @@
 #!/usr/bin/env bash
 
-rioworkers=${3:-`sysctl -n kern.smp.cpus`}
-INPUT_DIR=${1:-/data/mapping/AW3D30}
-OUTPUT_DIR=${2:-/data/mapping/AW3D30}
+# exit script on error
+set -e
+# exit on undeclared variable
+set -u
+
+
+## Color Constants
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Cyan='\033[0;36m'         # Cyan
+
+#set Defaults
+JAXA_DIR=/data/mapping/AW3D30
+OUTPUT_DIR=/data/mapping
+HIGHRES_VRT=""
+RIOWORKERS=`sysctl -n kern.smp.cpus`
+RGBIFY_OPTIONS=""
+GDALOPTIONS=""
+
+# print a help message
+function print_usage() {
+  echo "Usage: ${0} --input-dir ./ --output-dir ./ --workers 5"
+  echo "  --jaxa-dir:    the directory containing the jaxa DSM tiffs defaults" 
+  echo "                   to ${JAXA_DIR}"
+  echo "  --highres-vrt: a vrt file containing localized high res dems, order"
+  echo "                   is important, higest res of any given geo should be "
+  echo "                   last in the file, defaults to ${HIGHRES_VRT:-none}"
+  echo "  --output-dir:  the directory to put the resulting whole combined "
+  echo "                   raster tile defaults to ${OUTPUT_DIR}."
+  echo "  --workers:     the number of rgbify workers to run, defaults to the number of"
+  echo "                   CPU thread's detected (${RIOWORKERS})"
+  echo "  --verbose:     Turn on chattier output"
+  echo "  --overwrite:   Force the creation of all files, not just the missing/new ones"
+  exit
+}
+
+# print a given text entirely in a given color
+function color_echo () {
+    color=$1
+    text=$2
+    echo -e "${color}${text}${Color_Off}"
+}
+
+
+
+if [ "$#" -eq 0 ]; then
+    print_usage
+    exit 0
+fi
+echo $#
+
+while true; do
+  case "$1" in
+    -i | --input-dir)
+      if [ ! -n "${2-}" ]; then
+        INPUT_DIR=${2}
+        shift 2
+      else
+        shift 1
+     fi
+    ;;
+    -o | --output-dir)
+      if [ ! -n "${2-}" ]; then
+        OUTPUT_DIR="${2}"
+        shift 2
+      else
+        shift 1
+      fi
+    ;;
+    --highres-vrt)
+      if [ ! -n "${2-}" ]; then
+        HIGHRES_VRT="{$2}"
+        shift 2
+      else
+        shift 1
+      fi
+    ;;
+    -f | --overwrite)
+      OVERWRITE="true"
+      GDALOPTIONS=+"--overwrite "
+      shift 1
+    ;;
+    -v | --verbose)
+      VERBOSE="true"
+      RGBIFY_OPTIONS="--verbose"
+      shift 1
+    ;;
+    -q | --quiet)
+      QUIET="true"
+      RGBIFY_OPTIONS="--quiet"
+      shift 1
+    ;;
+    -h | -? | --help)
+      print_usage
+      break
+    ;;
+    -j | -w | --workers)
+      if [ ! -n "${2-}" ]; then
+        RIOWORKERS=${2}
+        shift 2
+      else
+        shift 1
+      fi
+    ;;
+    *)
+       break
+    ;;
+  esac
+done;
+
+
+
+
 vrtfile=${OUTPUT_DIR}/jaxa_tilergb0-12.vrt
 mbtiles=${OUTPUT_DIR}/jaxa_tilergb0-12.mbtiles
 vrtfile2=${OUTPUT_DIR}/jaxa_tilergb0-12-warp.tif
 
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
 
-find ${INPUT_DIR} -name \*_DSM.tif > ${OUTPUT_DIR}/JAXA_DSM.list
-#rm rio/*
-gdalbuildvrt -srcnodata -9999 -vrtnodata -9999 -input_file_list ${OUTPUT_DIR}/JAXA_DSM.list ${vrtfile}
-gdalwarp -r cubicspline -t_srs EPSG:3857 -dstnodata 0 -multi -co NUM_THREADS=ALL_CPUS -wo NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 50% -co COMPRESS=DEFLATE -co BIGTIFF=YES ${vrtfile} ${vrtfile2}
 
+find ${JAXA_DIR} -name \*_DSM.tif > ${OUTPUT_DIR}/JAXA_DSM.list
+echo ${HIGHRES_VRT} >> ${OUTPUT_DIR}/JAXA_DSM.list
+
+# need to add detection to determine if ANY of the files contained in 
+# JAXA_DSM.list are newer than the vrtfile but in the interm observe the overwrite flag
+
+gdalbuildvrt ${GDALOPTIONS} -resolution highest -srcnodata -9999 -vrtnodata -9999 -input_file_list ${OUTPUT_DIR}/JAXA_DSM.list ${vrtfile}
+
+if [ ${OVERWRITE} -eq "true" ] || [ ${vrtfile} -nt ${vrtfile2} ]; then
+  gdalwarp ${GDALOPTIONS} -r cubicspline -t_srs EPSG:3857 -dstnodata 0 -multi -co NUM_THREADS=ALL_CPUS -wo NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 50% -co COMPRESS=DEFLATE -co BIGTIFF=YES ${vrtfile} ${vrtfile2}
+fi
 
 #make use of rounding see details in https://github.com/mapbox/rio-rgbify/pull/34
 for n in 12 11 10 9 8 7 6 5 4; do
-    zoom=$((12-$n+4))
-    rio rgbify --verbose --base-val -10000 --interval 0.1 --min-z ${zoom} --max-z ${zoom} --workers ${rioworkers} --round-digits ${n} --format png ${vrtfile2} ${zoom}-tmp.mbtiles
- done
+  zoom=$((12-$n+4))
+  if [[ ${vrtfile2} -nt ${zoom}-tmp.mbtiles ]]; then
+    echo  ${vrtfile2} is newer than ${zoom}-tmp.mbtiles Generating
+    rio rgbify ${RGBIFY_OPTIONS} --base-val -10000 --interval 0.1 --min-z ${zoom} --max-z ${zoom} --workers ${RIOWORKERS} --round-digits ${n} --format png ${vrtfile2} ${zoom}-tmp.mbtiles
+  fi
+done
 
-cp ${zoom}-tmp.mbtile ${mbtiles}
-sqlite3 ${mbtiles} "
-ATTACH DATABASE '1-tmp.mbtiles' AS r1;
-ATTACH DATABASE '2-tmp.mbtiles' AS r2;
-ATTACH DATABASE '3-tmp.mbtiles' AS r3;
-ATTACH DATABASE '4-tmp.mbtiles' AS r4;
-ATTACH DATABASE '5-tmp.mbtiles' AS r5;
-ATTACH DATABASE '6-tmp.mbtiles' AS r6;
-ATTACH DATABASE '7-tmp.mbtiles' AS r7;
-ATTACH DATABASE '8-tmp.mbtiles' AS r8;
-ATTACH DATABASE '9-tmp.mbtiles' AS r9;
-ATTACH DATABASE '10-tmp.mbtiles' AS r10;
-ATTACH DATABASE '11-tmp.mbtiles' AS r11;
-ATTACH DATABASE '12-tmp.mbtiles' AS r12;
-REPLACE INTO tiles SELECT * FROM r1.tiles;
-REPLACE INTO tiles SELECT * FROM r2.tiles;
-REPLACE INTO tiles SELECT * FROM r3.tiles;
-REPLACE INTO tiles SELECT * FROM r4.tiles;
-REPLACE INTO tiles SELECT * FROM r5.tiles;
-REPLACE INTO tiles SELECT * FROM r6.tiles;
-REPLACE INTO tiles SELECT * FROM r7.tiles;
-REPLACE INTO tiles SELECT * FROM r8.tiles;
-REPLACE INTO tiles SELECT * FROM r9.tiles;
-REPLACE INTO tiles SELECT * FROM r10.tiles;
-REPLACE INTO tiles SELECT * FROM r11.tiles;
-REPLACE INTO tiles SELECT * FROM r12.tiles;
-CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);
-UPDATE metadata SET value = 'jaxa_terrainrgb_0-12' WHERE name = 'name' AND value = '';
-UPDATE metadata SET value = 'JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify' WHERE name = 'description';
-UPDATE metadata SET value = 'png' WHERE name = 'format';
-UPDATE metadata SET value = '1' WHERE name = 'version';
-UPDATE metadata SET value = 'baselayer' WHERE name = 'type';
-INSERT INTO metadata (name,value) VALUES(''attribution'',''<a href="https://earth.jaxa.jp/en/data/policy/">AW3D30 (JAXA)</a>'');
-PRAGMA journal_mode=DELETE;
-"
+#
+#
+#cp ${zoom}-tmp.mbtile ${mbtiles}
+#sqlite3 ${mbtiles} "
+#ATTACH DATABASE '1-tmp.mbtiles' AS r1;
+#ATTACH DATABASE '2-tmp.mbtiles' AS r2;
+#ATTACH DATABASE '3-tmp.mbtiles' AS r3;
+#ATTACH DATABASE '4-tmp.mbtiles' AS r4;
+#ATTACH DATABASE '5-tmp.mbtiles' AS r5;
+#ATTACH DATABASE '6-tmp.mbtiles' AS r6;
+#ATTACH DATABASE '7-tmp.mbtiles' AS r7;
+#ATTACH DATABASE '8-tmp.mbtiles' AS r8;
+#ATTACH DATABASE '9-tmp.mbtiles' AS r9;
+#ATTACH DATABASE '10-tmp.mbtiles' AS r10;
+#ATTACH DATABASE '11-tmp.mbtiles' AS r11;
+#ATTACH DATABASE '12-tmp.mbtiles' AS r12;
+#REPLACE INTO tiles SELECT * FROM r1.tiles;
+#REPLACE INTO tiles SELECT * FROM r2.tiles;
+#REPLACE INTO tiles SELECT * FROM r3.tiles;
+#REPLACE INTO tiles SELECT * FROM r4.tiles;
+#REPLACE INTO tiles SELECT * FROM r5.tiles;
+#REPLACE INTO tiles SELECT * FROM r6.tiles;
+#REPLACE INTO tiles SELECT * FROM r7.tiles;
+#REPLACE INTO tiles SELECT * FROM r8.tiles;
+#REPLACE INTO tiles SELECT * FROM r9.tiles;
+#REPLACE INTO tiles SELECT * FROM r10.tiles;
+#REPLACE INTO tiles SELECT * FROM r11.tiles;
+#REPLACE INTO tiles SELECT * FROM r12.tiles;
+#CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);
+#UPDATE metadata SET value = 'jaxa_terrainrgb_0-12' WHERE name = 'name' AND value = '';
+#UPDATE metadata SET value = 'JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify' WHERE name = 'description';
+#UPDATE metadata SET value = 'png' WHERE name = 'format';
+#UPDATE metadata SET value = '1' WHERE name = 'version';
+#UPDATE metadata SET value = 'baselayer' WHERE name = 'type';
+#INSERT INTO metadata (name,value) VALUES(''attribution'',''<a href="https://earth.jaxa.jp/en/data/policy/">AW3D30 (JAXA)</a>'');
+#PRAGMA journal_mode=DELETE;
+#"
--- download_zips.sh
+++ download_zips.sh
@@ -1,14 +1,165 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-mywget()
-{
-	wget --user 'jaxa_account' --password 'jaxa_password' --post-data 'username=jaxa_account&password=jaxa_password' "$1"
+# Darcy Buskermolen <darcy@dbitech.ca>
+
+# Script to download all the changed tiles of the AW3D30 dataset
+# uses both curl and wget
+
+# TODO
+# bounding box implementation needs to be completed
+# investigate allowing parralell fetches
+# clean up the arg handleing, make the output comandline defainable. 
+# script should have some additional sanity checks, and implement a --force flag
+
+
+
+help() {
+  echo
+  echo "Syntax: $0 [-h|v|bb]"
+  echo "options:"
+  echo "h     Print this help."
+  echo "v     Verbose mode."
+  echo "V     Print software version and exit."
+  echo "f     Pass the filename to program."
+  echo "b     BoundingBox to fetch."
+  echo "         $0 -b \"-178.2,6.6,-49.0,83.3\""
+  echo
 }
 
-export -f mywget
+download() {
+    # I've noticed that non-existant tiles return a 302 response code.
+    # We will only fetch the data that has a url returning a 200 response code.
+    local filename=$1
+    local url=https://www.eorc.jaxa.jp/ALOS/aw3d30/data/release_v2012/${filename}
+    echo Cheking $url
+    local respcode=$(curl -o /dev/null --silent -Iw '%{http_code}' $url)
+    echo Response code:  $respcode
+    if [ $respcode -eq 200 ]; then
+        echo Downloading...
+        wget --content-disposition -c -N -q --show-progress $url
+    fi
+    if [ ! -z "${RUNPROCESS}" ]; then
+      doProcess ${filename}
+    fi
+}
 
-# run wget in parallel using 8 thread/connection
-xargs -P 8 -n 1 -I {} bash -c "mywget '{}'" < file_list_zip.txt
+doProcess() {
+  local filename=$1
+  retval=`${RUNPROCESS} ${filename}`
+  return $?
+}
 
-#unzip the DSM tif files
-unzip -j download/\*.zip "*_DSM.tif" -d input/
+scaleup() {
+  local DIRECTION=${2}
+  local VAR=(${1//./ })
+  local INT=${VAR[0]}
+  if [ $[INT] -lt 0 ]; then
+    INT=$((INT*-1))
+  fi
+  if [ -z ${VAR[1]} ]; then
+    INT=$((INT+1))
+  fi   
+  local RES=$((INT/5))
+  if [ $((INT%5)) -ne 0 ]; then
+    RES=$((RES+1))
+  fi
+  return "$((RES*5))"
+}
+
+# Northern Hemisphere
+doNorth() {
+  for (( lat=0; lat<=${NORTH}; lat=lat+5 )) do
+    # North-East corner
+    for (( lon=0; lon<=$EAST; lon=lon+5 )) do
+        file="N$(printf '%03d' $lat)E$(printf '%03d' $lon)_N$(printf '%03d' $((10#$lat + 05)))E$(printf '%03d' $((10#$lon + 05))).zip"
+        download $file
+    done
+    # North-West corner
+    for (( lon=0; lon <=$WEST; lon=lon+5 )) do
+        if [ $lon -eq 005 ]; then
+            secondLonHem="E"
+        else
+            secondLonHem="W"
+        fi
+        file="N$(printf '%03d' $lat)W$(printf '%03d' $lon)_N$(printf '%03d' $((10#$lat + 05)))${secondLonHem}$(printf '%03d' $((10#$lon - 05))).zip"
+        download $file
+    done
+  done
+}
+
+# Southern Hemisphere
+doSouth() {
+  for (( lat=5; lat<=$SOUTH; lat=lat+5 )) do
+    # South-East corner
+    for (( lon=000; lon<=$EAST; lon=lon+5 )) do
+        if [ $lat -eq 005 ]; then
+            secondLatHem="N"
+        else
+            secondLatHem="S"
+        fi
+        file="S$(printf '%03d' $lat)E$(printf '%03d' $lon)_${secondLatHem}$(printf '%03d' $((10#$lat - 05)))E$(printf '%03d' $((10#$lon + 05))).zip"
+        download $file
+    done
+    # South-West corner
+    for (( lon=5; lon<=$WEST; lon=lon+5 )) do
+      if [ $lat -eq 005 ]; then
+            secondLatHem="N"
+        else
+            secondLatHem="S"
+        fi
+        if [ $lon -eq 005 ]; then
+            secondLonHem="E"
+        else
+            secondLonHem="W"
+        fi
+        file="S$(printf '%03d' $lat)W$(printf '%03d' $lon)_${secondLatHem}$(printf '%03d' $((10#$lat - 05)))${secondLonHem}$(printf '%03d' $((10#$lon - 05))).zip"
+        download $file
+    done
+  done
+}
+
+
+WEST=180
+EAST=175
+SOUTH=85
+NORTH=80
+
+while getopts ":hvVb:f:" option; do
+  case "${option}" in
+    h)
+      help
+      exit;;
+    v)
+      VERBOSE=1;;
+    V)
+      echo ${VERSION}
+      exit;;
+    f)
+      RUNPROCESS=${OPTARG};;
+    b)
+      #BBox has the format W,S,E,N
+      BBOX=(${OPTARG//,/ })
+      WESTB=${BBOX[0]}
+      NORTHB=${BBOX[3]}
+      EASTB=${BBOX[2]}
+      SOUTHB=${BBOX[1]}
+
+      echo "North ${NORTHB} East ${EASTB} South ${SOUTHB} West ${WESTB}"
+      
+      scaleup ${WESTB} W
+      WEST=$?
+      scaleup ${EASTB} E
+      EAST=$?
+      scaleup ${NORTHB} N
+      NORTH=$?
+      scaleup ${SOUTHB} S
+      SOUTH=$?
+      echo "North ${NORTH} East ${EAST} South ${SOUTH} West ${WEST}";;
+    \?)
+      echo "Error: Invalid option"
+      exit;;
+  esac
+done
+
+doNorth
+doSouth
