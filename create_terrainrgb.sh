6e4926e890194345a3f9d92da27f03337a1aea26
acalcutt <acalcutt@techidiots.net>
2023-01-01 14:28:51 -0500

    tweaks based on testting
    1.) zoom level 1/round 15 was missing.
    
    2.) Attach was not able to attach more than 10 databases at once
    
    3.) quote issues with metadata. just put this back to how it was because I didn't want to deal with it.

--- create_terrainrgb.sh
+++ create_terrainrgb.sh
@@ -8,49 +8,31 @@ vrtfile2=${OUTPUT_DIR}/jaxa_tilergb0-12_warp.vrt
 
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
 
-#rm rio/*
 gdalbuildvrt -overwrite -srcnodata -9999 -vrtnodata -9999 ${vrtfile} ${INPUT_DIR}/*_DSM.tif
 gdalwarp -r cubicspline -t_srs EPSG:3857 -dstnodata 0 -co COMPRESS=DEFLATE ${vrtfile} ${vrtfile2}
 
 #make use of rounding see details in https://github.com/mapbox/rio-rgbify/pull/34
-for n in 4 5 6 7 8 9 10 11 12 13 14 16; do
+for n in 4 5 6 7 8 9 10 11 12 13 14 15 16; do
     zoom=$((12-$n+4))
-    rio rgbify -b -10000 -i 0.1 --min-z ${zoom} --max-z ${zoom} -j 24 --round-digits ${n} --format png ${vrtfile2} ${zoom}-tmp.mbtile
- done
+    rio rgbify -b -10000 -i 0.1 --min-z ${zoom} --max-z ${zoom} -j 24 --round-digits ${n} --format png ${vrtfile2} ${OUTPUT_DIR}/${zoom}-tmp.mbtiles
+done
 
-cp ${zoom}-tmp.mbtile ${mbtiles}
+cp ${OUTPUT_DIR}/0-tmp.mbtiles ${mbtiles}
+
+for n in 1 2 3 4 5 6 7 8 9 10 11 12; do
+echo "copying '${n}'"
 sqlite3 ${mbtiles} "
-ATTACH DATABASE '0-tmp.mbtiles' AS r0;
-ATTACH DATABASE '1-tmp.mbtiles' AS r1;
-ATTACH DATABASE '2-tmp.mbtiles' AS r2;
-ATTACH DATABASE '3-tmp.mbtiles' AS r3;
-ATTACH DATABASE '4-tmp.mbtiles' AS r4;
-ATTACH DATABASE '5-tmp.mbtiles' AS r5;
-ATTACH DATABASE '6-tmp.mbtiles' AS r6;
-ATTACH DATABASE '7-tmp.mbtiles' AS r7;
-ATTACH DATABASE '8-tmp.mbtiles' AS r8;
-ATTACH DATABASE '9-tmp.mbtiles' AS r9;
-ATTACH DATABASE '10-tmp.mbtiles' AS r10;
-ATTACH DATABASE '11-tmp.mbtiles' AS r11;
-REPLACE INTO tiles SELECT * FROM r0.tiles;
-REPLACE INTO tiles SELECT * FROM r1.tiles;
-REPLACE INTO tiles SELECT * FROM r2.tiles;
-REPLACE INTO tiles SELECT * FROM r3.tiles;
-REPLACE INTO tiles SELECT * FROM r4.tiles;
-REPLACE INTO tiles SELECT * FROM r5.tiles;
-REPLACE INTO tiles SELECT * FROM r6.tiles;
-REPLACE INTO tiles SELECT * FROM r7.tiles;
-REPLACE INTO tiles SELECT * FROM r8.tiles;
-REPLACE INTO tiles SELECT * FROM r9.tiles;
-REPLACE INTO tiles SELECT * FROM r10.tiles;
-REPLACE INTO tiles SELECT * FROM r11.tiles;
-CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);
-UPDATE metadata SET value = "jaxa_terrainrgb_0-12" WHERE name = "name" AND value = "";
-UPDATE metadata SET value = "JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify" WHERE name = "description";
-UPDATE metadata SET value = "png" WHERE name = "format";
-UPDATE metadata SET value = "1" WHERE name = "version";
-UPDATE metadata SET value = "baselayer" WHERE name = "type";
-INSERT INTO metadata (name,value) VALUES(''attribution'',''<a href="https://earth.jaxa.jp/en/data/policy/">AW3D30 (JAXA)</a>'');
-PRAGMA journal_mode=DELETE;
+ATTACH DATABASE '${OUTPUT_DIR}/${n}-tmp.mbtiles' AS r${n};
+REPLACE INTO tiles SELECT * FROM r${n}.tiles;
+DETACH DATABASE 'r${n}';
 "
+done
 
+sqlite3 ${mbtiles} 'CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "jaxa_terrainrgb_0-12" WHERE name = "name" AND value = "";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "JAXA ALOS World 3D 30m (AW3D30) converted with gdal and rio rgbify" WHERE name = "description";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "png" WHERE name = "format";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
+sqlite3 ${mbtiles} 'INSERT INTO metadata (name,value) VALUES(''attribution'',''<a href="https://earth.jaxa.jp/en/data/policy/">AW3D30 (JAXA)</a>'');'
+sqlite3 ${mbtiles} 'PRAGMA journal_mode=DELETE;'
