20f90ab68cb745912bf4da90dbc623a6b2f40589
Andrew Calcutt <acalcutt@techidiots.net>
2023-09-04 11:24:10 -0400

    lower default max_zoom to 11
    In testing, there didn't seem to be much difference between 11 and 12 in hillshade/terrain with this dataset. This change results in a much smaller mbtiles file.

--- create_terrainrgb.sh
+++ create_terrainrgb.sh
@@ -2,24 +2,23 @@
 
 INPUT_DIR=./input
 OUTPUT_DIR=./output
-vrtfile=${OUTPUT_DIR}/jaxa_tilergb0-12.vrt
-mbtiles=${OUTPUT_DIR}/jaxa_tilergb0-12.mbtiles
-vrtfile2=${OUTPUT_DIR}/jaxa_tilergb0-12_warp.vrt
+vrtfile=${OUTPUT_DIR}/jaxa_terrainrgb0-11.vrt
+mbtiles=${OUTPUT_DIR}/jaxa_terrainrgb0-11.mbtiles
+vrtfile2=${OUTPUT_DIR}/jaxa_terrainrgb0-11_warp.vrt
 
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
 
 #rm rio/*
 gdalbuildvrt -overwrite -srcnodata -9999 -vrtnodata -9999 ${vrtfile} ${INPUT_DIR}/*_DSM.tif
 gdalwarp -r cubicspline -t_srs EPSG:3857 -dstnodata 0 -co COMPRESS=DEFLATE ${vrtfile} ${vrtfile2}
-rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 12 -j 24 --format png ${vrtfile2} ${mbtiles}
+rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 11 -j 24 --format png ${vrtfile2} ${mbtiles}
 
 sqlite3 ${mbtiles} 'CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);'
-sqlite3 ${mbtiles} 'UPDATE metadata SET value = "jaxa_terrainrgb_0-12" WHERE name = "name" AND value = "";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "jaxa_terrainrgb_0-11" WHERE name = "name" AND value = "";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify" WHERE name = "description";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "png" WHERE name = "format";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
 sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('attribution','<a href=""https://earth.jaxa.jp/en/data/policy/"">AW3D30 (JAXA)</a>');"
 sqlite3 ${mbtiles} 'PRAGMA journal_mode=DELETE;'
-
 
