6adc9ab49b0ff6dee02c877b4bfc86dcad199644
Eric Theise <erictheise@gmail.com>
2024-08-03 18:54:29 -0700

    Typos fixed.

--- create_contour_100_parallel.sh
+++ create_contour_100_parallel.sh
@@ -9,26 +9,26 @@ prun()
 	INPUT_DIR=./input
 	JSON_OUTPUT_DIR=/opt/jaxa_AW3D30_to_MBTiles/json_100
 	SHP_OUTPUT_DIR=/opt/jaxa_AW3D30_to_MBTiles/json_100/shp
-	
+
 	[ -d "$JSON_OUTPUT_DIR" ] || mkdir -p $JSON_OUTPUT_DIR || { echo "error: $JSON_OUTPUT_DIR " 1>&2; exit 1; }
 	[ -d "$SHP_OUTPUT_DIR" ] || mkdir -p $SHP_OUTPUT_DIR || { echo "error: $SHP_OUTPUT_DIR " 1>&2; exit 1; }
 
 	filename="$INPUT_DIR/$1"
 	json="$JSON_OUTPUT_DIR/$(basename -s .tif $filename)_contour.geojson"
 	shp="$SHP_OUTPUT_DIR/$(basename -s .tif $filename)_contour.shp"
-	
+
 	echo "$filename - $json - $shp"
 
-	#create countor shape
-	#gdal_contour -a elev -i 100 -f "GeoJSON" ${filename} ${json} #note - this geojson did not work right with tippcanoe. output came out one long line.
+	#create contour shape
+	#gdal_contour -a elev -i 100 -f "GeoJSON" ${filename} ${json} #note - this geojson did not work right with tippecanoe. output came out one long line.
 	gdal_contour -a elev -i 100 ${filename} ${shp}
-	
+
 	#convert to geojson
 	ogr2ogr -f GeoJSON ${json} ${shp}
-	
+
 	#rm= "$SHP_OUTPUT_DIR/$(basename -s .tif $filename)_contour*"
 	find "$SHP_OUTPUT_DIR" -type f -name "$(basename -s .tif $filename)*" -delete
-	
+
 
 }
 
--- create_hs_cr_images_parallel.sh
+++ create_hs_cr_images_parallel.sh
@@ -14,7 +14,7 @@ prun()
 {
 
 	filename="input/$1"
-	
+
 	hs=${OUTPUT_DIR}/"$(basename -s .tif $filename)_HS.tif"
 	hso=${OUTPUT_DIR}/"$(basename -s .tif $filename)_HSO.tif"
 	hsc=${HSC_OUTPUT_DIR}/"$(basename -s .tif $filename)_HSC.tif"
@@ -24,21 +24,21 @@ prun()
 	mgeo=${OUTPUT_DIR}/"$(basename -s .tif $filename)_MG.geo"
 	fint=${OUTPUT_DIR}/"$(basename -s .tif $filename)_FINT.tif"
 	finc=${FIN_OUTPUT_DIR}/"$(basename -s .tif $filename)_FIN.tif"
-	
+
 	echo $filename
 	#produce a transparent hillshade image
 	gdaldem hillshade -of GTiff $filename $hs -alpha -s 111120 -z 5 -az 315 -alt 60 -compute_edges
 	gdaldem color-relief $hs -alpha hillshade.ramp $hso
 	gdal_translate -co COMPRESS=LZW -co ALPHA=YES $hso $hsc
 	rm $hs
-	
 
-	#create color releif image
+
+	#create color relief image
 	gdaldem color-relief -of GTiff $filename -alpha shade.ramp $cr
 	gdal_translate -a_nodata 0 -co COMPRESS=LZW -co ALPHA=YES $cr $crc
-	
 
-	#create color releif image with hillshade
+
+	#create color relief image with hillshade
 	composite -gravity Center $hsc $crc -alpha Set $mtif
 	listgeo $filename > $mgeo #Dump geotiff metadata from a file that has it.
 	geotifcp -g $mgeo $mtif $fint #merge the metadata into the composite image
